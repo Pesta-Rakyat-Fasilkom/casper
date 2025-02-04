46750ab10d3a85ec4efe6d1bed49b36e0b3bf438
secona <77039267+secona@users.noreply.github.com>
2025-02-04 10:26:05 +0700

    feat: divisi creative

---
+++ modules/recruitment/elements/creative.tsx
@@ -0,0 +1,34 @@
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+
+export const Creative = () => {
+  return (
+    <div>
+      <h2 className="text-2xl">Divisi Kreatif</h2>
+
+      <p>
+        Divisi Kreatif di PERAK 2025 adalah tempat bagi ekspresi visual dan artistik. Divisi ini bertanggung jawab menciptakan identitas visual, mendokumentasikan momen penting, dan memastikan setiap acara tampil menarik. Dengan berbagai peran di dalamnya, divisi ini menjadi wadah bagi individu kreatif untuk berkarya.
+      </p>
+
+      <Accordion type="single">
+        <AccordionItem value="visual-design-visdes">
+          <AccordionTrigger>Visual Design (Visdes)</AccordionTrigger>
+          <AccordionContent>
+            Visdes mengelola seluruh aspek visual yang mendukung branding dan komunikasi Perak, mulai dari logo, poster, hingga konten digital. Visdes memastikan tampilan acara tetap selaras dan menarik melalui desain yang estetis dan kreatif.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="documentation-dnc">
+          <AccordionTrigger>Documentation (DNC)</AccordionTrigger>
+          <AccordionContent>
+            Deskripsi Divisi 2
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="documentation-dnc">
+          <AccordionTrigger>Dekorasi (Dekor)</AccordionTrigger>
+          <AccordionContent>
+            Dekor bertanggung jawab atas konsep dekorasi yang mendukung tema acara. Dari panggung hingga elemen dekoratif lainnya, Dekor menciptakan suasana yang mendukung pengalaman acara yang lebih berkesan.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </div>
+  );
+};
--- modules/recruitment/index.tsx
+++ modules/recruitment/index.tsx
@@ -1,25 +1,12 @@
 "use client";
 
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+import { Creative } from "./elements/creative";
 
 export const Recruitment = () => {
   return (
     <div className="mx-auto px-4 my-12 max-w-5xl">
-      <h1 className="text-xl text-center">Recruitment is open!</h1>
-      <Accordion type="single">
-        <AccordionItem value="item-1">
-          <AccordionTrigger>Divisi 1</AccordionTrigger>
-          <AccordionContent>
-            Deskripsi Divisi 1
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="item-2">
-          <AccordionTrigger>Divisi 2</AccordionTrigger>
-          <AccordionContent>
-            Deskripsi Divisi 2
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
+      <h1 className="text-4xl text-center">Recruitment is open!</h1>
+      <Creative />
     </div>
   );
 }
