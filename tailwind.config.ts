94b62b1a135431f7090851a068368d8cfebb6561
kimnzh <hakimnizami15@gmail.com>
2025-02-05 23:13:50 +0700

    feat: added ds colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,43 +20,109 @@ const config = {
     },
     extend: {
       fontFamily: {
-        'husky-stash': 'var(--font-husky-stash)',
-        geist: 'var(--font-geist)',
+        "husky-stash": "var(--font-husky-stash)",
+        geist: "var(--font-geist)",
       },
       colors: {
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
+        accents: {
+          purple: {
+            1: "#240034",
+            2: "#3C0056",
+            3: "#480068",
+            4: "#500073",
+            5: "#C9B0D4",
+          },
+          peach: {
+            1: "#733652",
+            2: "#BF5989",
+            3: "#CC5F92",
+            4: "#FF77B7",
+            5: "#FFD5E9",
+          },
+          red: {
+            1: "#6F1026",
+            2: "#941633",
+            3: "#C51D44",
+            4: "#F62455",
+            5: "#FCBBCA",
+          },
+          pink: {
+            1: "#73476A",
+            2: "#BF77B0",
+            3: "#CC7EBC",
+            4: "#FF9EEB",
+            5: "#FFE1F9",
+          },
+          yellow: {
+            1: "#BFAB71",
+            2: "#CCB679",
+            3: "#E6CD88",
+            4: "#FFE497",
+            5: "#FFF7DF",
+          },
+          grey: {
+            1: "#ABABAB",
+            2: "#CDCDCD",
+            3: "#E4E4E4",
+            4: "#F7F7F7",
+            5: "#FBFBFB",
+          },
+          blue: {
+            1: "#172252",
+            2: "#1F2E6E",
+            3: "#2A3D92",
+            4: "#344CB7",
+            5: "#C0C8E9",
+          },
+          green: {
+            1: "#4B5A1A",
+            2: "#647923",
+            3: "#85A12E",
+            4: "#A6C93A",
+            5: "#E3EEC2",
+          },
         },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
+        text: {
+          dark: {
+            1: "#340717",
+            2: "#590C28",
+            3: "#700F32",
+          },
+          light: {
+            1: "#E5D9EA",
+            2: "#F7F3F8",
+            3: "#FCFBFD",
+          },
         },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
+        section: {
+          1: "#90CAF9",
+          2: "#19369C",
+          3: "#694AF0",
+          4: "#520245",
+          5: "#EC7842",
         },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
+        background: {
+          1: "#FFDAFC",
+          2: "#F991CC",
+          3: "#FDFEF0",
+          4: "#DFD9CB",
+          5: "#E2F3F4",
+          6: "#6DC7D1",
         },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
+        button: {
+          primary: "#F62455",
+          secondary: "#A6C93A",
         },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
+      },
+      backgroundImage: {
+        "section-6": "linear-gradient(#9865ED, #EC7842)",
+        "section-7": "linear-gradient(#90CAF9, #694AF0)",
+        "section-8": "linear-gradient(#FFEFD8, #F4B020, #EC7842)",
+        "button-chips": "linear-gradient(to bottom right, #F62455, #901532)",
+        "icons-success": "linear-gradient(#36823F, #0B5414)",
+        "icons-warning": "linear-gradient(#0B5414, #ECA122)",
+        "icons-error": "linear-gradient(#CB5353, #9A0101)",
+        "icons-loading": "linear-gradient(to bottom right, #694AF0,#90CAF9)",
       },
       borderRadius: {
         lg: "var(--radius)",
