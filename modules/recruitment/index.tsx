f1b9eadcb68713876c0f4bac628858047c96dd1b
secona <77039267+secona@users.noreply.github.com>
2025-02-05 18:59:45 +0700

    feat: buttons

--- modules/recruitment/index.tsx
+++ modules/recruitment/index.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Creative } from "./elements/creative";
 import { Finance } from "./elements/finance";
@@ -12,6 +14,20 @@ export const Recruitment = () => {
   return (
     <div className="mx-auto px-4 my-12 max-w-5xl flex flex-col gap-12">
       <h1 className="text-4xl text-center">PERAK 2025 is open for recuruitment!</h1>
+
+      <div className="flex flex-row gap-4 justify-center">
+        <Button asChild variant='outline'>
+          <Link href="https://forms.gle/3BQsnTqoF4GfTASd6" rel="noopener noreferrer" target="_blank">
+            Form Open Recruitment
+          </Link>
+        </Button>
+
+        <Button asChild variant='outline'>
+          <Link href="https://forms.gle/QBcxBLpaWrZ6QH1b9" rel="noopener noreferrer" target="_blank">
+            Form Pengumpulan Tugas
+          </Link>
+        </Button>
+      </div>
 
       <Tabs>
         <TabsList className="mb-5 align-center">
