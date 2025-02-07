e28b56ac8a3e08bf78ce84cffd9cfe5368375daa
ryuun1corn <yuda.arif.prasojo@gmail.com>
2025-02-07 09:12:56 +0700

    feat: implement toaster

--- components/elements/Layout/index.tsx
+++ components/elements/Layout/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Footer } from "./Footer";
 import { Navbar } from "./Navbar";
+import { Toaster } from "@/components/ui/toaster";
 
 export const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <Navbar />
+      <Toaster />
       <main className="w-full min-h-screen pt-[80px]">{children}</main>
       <Footer />
     </>
--- components/ui/toaster.tsx
+++ components/ui/toaster.tsx
@@ -1,6 +1,5 @@
-"use client"
-
-import { useToast } from "@/components/hooks/use-toast"
+"use client";
+import { useToast } from "@/components/hooks/use-toast";
 import {
   Toast,
   ToastClose,
@@ -8,28 +7,58 @@ import {
   ToastProvider,
   ToastTitle,
   ToastViewport,
-} from "@/components/ui/toast"
+} from "@/components/ui/toast";
+import {
+  CheckCircle,
+  AlertCircle,
+  XCircle,
+  LucideIcon,
+  Loader2,
+} from "lucide-react";
+
+const variantIcons: Record<string, LucideIcon> = {
+  success: CheckCircle,
+  warning: AlertCircle,
+  error: XCircle,
+  loading: Loader2,
+};
 
 export function Toaster() {
-  const { toasts } = useToast()
+  const { toasts } = useToast();
 
   return (
     <ToastProvider>
-      {toasts.map(function ({ id, title, description, action, ...props }) {
+      {toasts.map(function ({
+        id,
+        title,
+        description,
+        action,
+        variant = "success",
+        ...props
+      }) {
+        const Icon = variantIcons[variant ?? "success"];
+
         return (
-          <Toast key={id} {...props}>
-            <div className="grid gap-1">
-              {title && <ToastTitle>{title}</ToastTitle>}
-              {description && (
-                <ToastDescription>{description}</ToastDescription>
+          <Toast key={id} {...props} variant={variant}>
+            <div className="flex items-center gap-3">
+              {Icon && (
+                <Icon
+                  className={`h-8 w-8 ${variant === "loading" ? "animate-spin" : ""}`}
+                />
               )}
+              <div className="grid gap-1">
+                {title && <ToastTitle>{title}</ToastTitle>}
+                {description && (
+                  <ToastDescription>{description}</ToastDescription>
+                )}
+              </div>
             </div>
             {action}
             <ToastClose />
           </Toast>
-        )
+        );
       })}
       <ToastViewport />
     </ToastProvider>
-  )
+  );
 }
