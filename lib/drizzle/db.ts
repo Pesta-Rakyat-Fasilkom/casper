7a37477b11de7cd8a4b3a45891ae29396f6a2f0e
ryuun1corn <yuda.arif.prasojo@gmail.com>
2025-02-09 01:37:57 +0700

    try: new users schema

--- lib/drizzle/db.ts
+++ lib/drizzle/db.ts
@@ -1,5 +1,9 @@
-import { drizzle } from 'drizzle-orm/postgres-js'
-import postgres from 'postgres'
+import { drizzle } from "drizzle-orm/postgres-js";
+import { users } from "./schema";
+import postgres from "postgres";
 
-export const client = postgres(process.env.DATABASE_URL!, { prepare: false })
+export const client = postgres(process.env.DATABASE_URL!, { prepare: false });
 export const db = drizzle(client);
+
+const allUsers = await db.select().from(users);
+console.log(allUsers);
--- lib/drizzle/schema.ts
+++ lib/drizzle/schema.ts
@@ -1,5 +1,5 @@
-import { pgSchema, pgTable, uuid, varchar } from 'drizzle-orm/pg-core';
-
+import { pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
+/**
 const authSchema = pgSchema('auth');
 
 const users = authSchema.table('users', {
@@ -9,4 +9,11 @@ const users = authSchema.table('users', {
 export const profiles = pgTable('profiles', {
   id: uuid('id').primaryKey().references(() => users.id),
   fullname: varchar('fullname', { length: 255 }),
+});
+*/
+
+export const users = pgTable("users", {
+  id: serial("id").primaryKey(),
+  fullName: text("full_name"),
+  phone: varchar("phone", { length: 256 }),
 });
