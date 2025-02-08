f33e89f1c26dae5dc664a78ed35fa5d9d9fb41c6
ryuun1corn <yuda.arif.prasojo@gmail.com>
2025-02-09 02:56:19 +0700

    feat: implement db schema

--- lib/drizzle/db.ts
+++ lib/drizzle/db.ts
@@ -1,9 +1,5 @@
 import { drizzle } from "drizzle-orm/postgres-js";
-import { users } from "./schema";
 import postgres from "postgres";
 
 export const client = postgres(process.env.DATABASE_URL!, { prepare: false });
 export const db = drizzle(client);
-
-const allUsers = await db.select().from(users);
-console.log(allUsers);
